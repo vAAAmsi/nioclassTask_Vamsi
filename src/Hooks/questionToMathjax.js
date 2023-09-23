149f30baf33736e2c4874b2645195d592ffa516a
vAAAmsi <103809854+vAAAmsi@users.noreply.github.com>
2023-09-23 14:50:51 +0530

    Update questionToMathjax.js

--- src/Hooks/questionToMathjax.js
+++ src/Hooks/questionToMathjax.js
@@ -6,14 +6,14 @@ function MathQuestion( {question, index} ) {
     const mathjaxContext = {
     input: 'tex',
     output: 'html',
-    display: true, 
+    display: false, 
   };
 
   
   const processedQuestionText = question.replace(/\$(.*?)\$/g, '\\($1\\)');
 
   return (
-    <MathJaxContext {...mathjaxContext} inline dynamic>
+    <MathJaxContext {...mathjaxContext} >
       <div style={{color : 'black'}}> <span style={{color : 'red'}}>Question no {index+1} : </span>
         <MathJax>{processedQuestionText}</MathJax>
       </div>
