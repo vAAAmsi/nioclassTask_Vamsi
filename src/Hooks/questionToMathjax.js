e26d3e1fd9c56c681738c90f8d0e47fd8e9cc2bf
vamsi <mamillapallivamsi11@gmail.com>
2023-09-23 15:16:34 +0530

    mathjax

--- src/Hooks/questionToMathjax.js
+++ src/Hooks/questionToMathjax.js
@@ -4,16 +4,18 @@ import { MathJax, MathJaxContext } from 'better-react-mathjax';
 function MathQuestion( {question, index} ) {
    
     const mathjaxContext = {
+    
     input: 'tex',
     output: 'html',
     display: true, 
+    
   };
 
   
   const processedQuestionText = question.replace(/\$(.*?)\$/g, '\\($1\\)');
 
   return (
-    <MathJaxContext {...mathjaxContext}>
+    <MathJaxContext {...mathjaxContext} >
       <div style={{color : 'black'}}> <span style={{color : 'red'}}>Question no {index+1} : </span>
         <MathJax>{processedQuestionText}</MathJax>
       </div>
